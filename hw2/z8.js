8ea6f410567899817dcb4ad8f7248abe750ead2b
Aleksandr Avrakhov <aleksandr.avrakhov@privatbank.ua>
2022-01-19 17:53:57 +0200

    Update z8.js
    Refac

--- hw2/z8.js
+++ hw2/z8.js
@@ -3,16 +3,6 @@
 на экран его элементы. Обязательно нужно использовать рекурсию. Использовать цикл запрещено. Данная функция должна обязательно содержать проверку входного 
 параметра, потому что принимать она может только не пустой массив.
 *Возможно вам понадобится метод [splice](https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Array/splice).*
-
-```js
-f([1,2,3]);
-// 1
-// 2
-// 3
-f(1,2,3) // Error: parameter type should be an array
-f('Content') // Error: parameter type should be an array
-f([]) // Error: parameter can't be an empty
-```
 */
 
 function f(array) {
